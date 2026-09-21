409a2dd6fa940c132b8ed4a34c34fa366f9c9fed
SamuelCS1314 <samuelcs1314@gmail.com>
2026-09-21 19:40:37 -0300

    Update critica.js

--- public/javaScript/critica.js
+++ public/javaScript/critica.js
@@ -5,28 +5,29 @@ function contadorSugestao() {
 }
 
 function criarJson(){
-   const formulario = {
+    const date = new Date();
+    const data = date.toLocalDateString("pt-br");
+    
+   const formCritica = {
       setor :    document.getElementById('setor').value,
+      data:      data,
       assunto :  document.getElementById('assunto').value,
       descricao: document.getElementById('descricao').value,
       sugestao:  document.getElementById('sugestao').value, 
       nome:      document.getElementById('nome').value
     };
     
-    console.log(formulario);
+    console.log(formCritica);
 
-    const jsonString = JSON.stringify(formulario, null, 2);
+    const jsonString = JSON.stringify(formCritica, null, 2);
 
-    // 3. Criar o arquivo para download no navegador
     const blob = new Blob([jsonString], { type: 'application/json' });
     const url = URL.createObjectURL(blob);
     const link = document.createElement('a');
 
     link.href = url;
-    link.download = 'formulario.json'; // Nome do arquivo sugerido
-    link.click();
+    link.download = `formCritica${data}.json`; link.click();
 
-    // Limpar a memória
     URL.revokeObjectURL(url);
     console.log('Download do JSON iniciado com sucesso!');
 }
