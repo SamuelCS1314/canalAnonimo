049de4f69286a6b683024308be04f1a3a0d0384c
SamuelCS1314 <samuelcs1314@gmail.com>
2026-09-26 17:31:02 -0300

    Divisão do CSS Universal e Indexido

--- api.js
+++ api.js
@@ -12,9 +12,9 @@ import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from "url";
 
-const upload = multer({dest: 'uploads/' });
+const upload = multer({ dest: 'uploads/' });
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename); 
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 import db from './db.js';
@@ -24,24 +24,24 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public',  'html', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
 });
 
 
 app.post('/api/uploads/criticas', async (req, res) => {
-    try{
-        const  { setor, data, assunto, descricao, sugestao, nome } = req.body;
-        
+    try {
+        const { setor, data, assunto, descricao, sugestao, nome } = req.body;
+
         const query = 'INSERT INTO critica (setor, data, assunto, descricao, sugestao, nome) VALUES (?, ?, ?, ?, ?, ?)';
-        const [resultado] =  await db.query(query, [setor, data, assunto, descricao, sugestao, nome]);
-        
+        const [resultado] = await db.query(query, [setor, data, assunto, descricao, sugestao, nome]);
+
         const id = resultado.insertId
-        
-        res.status(201).json({ id: id, mensagem:'recebido com sucesso! ',})
-    }catch(error){
-    console.error('Erro no servidor: ', error);
-    res.status(500).json({ erro: 'Erro ao guardar crítica'});
-}
+
+        res.status(201).json({ id: id, mensagem: 'recebido com sucesso! ', })
+    } catch (error) {
+        console.error('Erro no servidor: ', error);
+        res.status(500).json({ erro: 'Erro ao guardar crítica' });
+    }
 });
 
 app.listen(PORT, () => console.log(`O servidor está rodando na porta localhost:${PORT} `));
